a498cf725e916b98f8d9f2fadebaaa28de25f883
Sij <sijibamgboye@yahoo.com>
2022-09-20 18:38:06 +0100

    update url on frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://aef102f88df28415a97d6ba1a0a69fc0-281672624.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
